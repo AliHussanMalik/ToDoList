cee9c1e9d3d8b4a78e00d5cb3b4ee092dd337bd6
Ali Hussan Malik <malikalihussan2@gmail.com>
2026-05-22 23:31:15 +0500

    JSON FLOW DONE
    pipeline is completed till the objs into arr and arr into the local storage. set and get.

--- todo.js
+++ todo.js
@@ -1,21 +1,61 @@
 function consoleIt(){
-    let Tapic = document.getElementById("topicId").value;
-    let Date = document.getElementById("dateid").value;
-    let Details = document.getElementById("detailsId").value;
-    console.log("Topic" , Tapic)
-    console.log("Date", Date)
-    console.log("Details", Details)
+    let Topicx = document.getElementById("topicId").value;
+    let Datex = document.getElementById("dateid").value;
+    let Detailsx = document.getElementById("detailsId").value;
+    console.log("Topic" , Topicx)
+    console.log("Date", Datex)
+    console.log("Details", Detailsx)
 
-    const ToDoObj ={
-        Topic:Tapic,
-        Date:Date,
-        Details:Details
+    alert("Pre Mature Alert 01")
+    function myObject(tpc,dt,dtl,...rest){
+        this.Topic = tpc;
+        this.Date = dt;
+        this.Details = dtl;
+        console.log(`Object In Function ${myObject}Topic: ${tpc}Date:${dt} Details:${dtl}`)
+        return;
     }
-    console.log(ToDoObj);
+    alert("Pre Mature Alert 02")    
+    const NewList = new myObject(Topicx,Datex,Detailsx);
+    console.log("Modern List",NewList);
 
-    const ToDoArr = []
-    ToDoarr.push(ToDoObj)
-    console.log(ToDoArr);
-    alert("asdf")
-    return;
+    alert("Pre Mature Alert 02.2")
+        const ToDoArr = []
+    ToDoArr.push(NewList)
+    
+
+    alert("Pre Object Alert00")
+    alert("In Between Object Alert 01")
+    console.log("To Do Array 00",ToDoArr);
+    console.log("To Do Array 01",ToDoArr[0]);
+    console.log("To Do Array 02",ToDoArr[1]);
+    alert("In Between Object Alert 01.1")
+    var ReterivedArray = localStorage.getItem("ProgrammerF");
+    console.log("ReterivedArray B4",ReterivedArray)
+    alert("In Between Object Alert 02")
+    if (ReterivedArray === null){
+        alert("In IF")
+        localStorage.setItem("ProgrammerF",JSON.stringify(NewList))
+        var ReterivedArray00 = JSON.parse(localStorage.getItem("ProgrammerF"))
+        var ReterivedArray01 = localStorage.getItem("ProgrammerF")
+        var ReterivedArray02 = localStorage.getItem.ProgrammerF
+        // var ReterivedArray03 = JSON.parse(localStorage.getItem("ProgrammerF"))
+        // var ReterivedArray = NewList
+        console.log("ReterivedArray00 After",ReterivedArray00)
+        console.log("ReterivedArray01 After",ReterivedArray01)
+        console.log("ReterivedArray02 After",ReterivedArray02)
+        // console.log("ReterivedArray03 After",ReterivedArray03)
+        alert("In Between Object Alert 03")
+    }
+    else{
+        alert("In Else")
+            var ReterivedArray = ReterivedArray.concat(JSON.stringify(NewList))
+        localStorage.setItem("ProgrammerF",ReterivedArray)
+        alert("CheckPoint")
+        var ReterivedArrayy =  localStorage.getItem("ProgrammerF")
+            console.log("ReterivedArray IZ",ReterivedArray);
+            alert("Be Alert")
+        }
+    alert("Post Array Alert")
+    return; 
 };
+
