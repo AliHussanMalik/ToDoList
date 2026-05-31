5bb20b18396df1c64ab8a05ceef3c181614ad202
Ali Hussan Malik <malikalihussan2@gmail.com>
2026-05-31 23:12:28 +0500

    Create_Tasks

--- todo.js
+++ todo.js
@@ -11,28 +11,74 @@ function consoleIt() {
   
     const NewList = new MyObject(Topicx, Datex, Detailsx);
 
-    let storedData = localStorage.getItem("ProgrammerF");
-    let currentArray = [];
+    let storedData = localStorage.getItem("Universal Key");
+    var currentArray = [];
 
     if (storedData === null) {
         currentArray.push(NewList);
     } else {
-        try {
             currentArray = JSON.parse(storedData);
             
             currentArray.push(NewList);
-        } catch (e) {
-            console.error("Error parsing JSON from localStorage, resetting array:", e);
-            currentArray = [NewList]; 
-        }
     }
 
-    // 4. Save the updated array back to localStorage as a JSON string
-    localStorage.setItem("ProgrammerF", JSON.stringify(currentArray));
+    localStorage.setItem("Universal Key", JSON.stringify(currentArray));
 
-    // 5. Loop through and print the updated array items safely
-    for (let i = 0; i < currentArray.length; i++) {
-        console.log(`Item ${i}: Topic: ${currentArray[i].Topic}, Date: ${currentArray[i].Date}`);
-    }
+    // for (let i = 0; i < currentArray.length; i++) {
+    //     console.log(`Item ${i}: Topic: ${currentArray[i].Topic}, Date: ${currentArray[i].Date}`);
+    // }
     return; 
+}
+
+const rawData = localStorage.getItem("Universal Key")
+
+if(rawData){
+    
+    const itemsArray = JSON.parse(rawData)
+
+    const container = document.getElementById('data-container');
+
+    
+    itemsArray.forEach((element ,index) => {
+        const newDiv = document.createElement("div");
+        newDiv.className = 'newDivClass'        
+        
+        const edt = document.createElement("button")
+        const del = document.createElement("button")
+
+        edt.textContent = "Edit";
+        del.textContent = "Delete";
+
+        edt.className = 'edit-button';
+        del.className = 'delete-button';
+        
+        edt.addEventListener("click", (topic,date, details)=>{
+            window.location.href="http://127.0.0.1:5500/todo.html";
+            const queryString = window.location.search;
+            const urlParams = new URLSearchParams(queryString)
+
+            const Topc = urlParams.get('Topic')
+            const Dat = urlParams.get('Date')
+            const Dtails = urlParams.get('Details')
+            console.log(`${Topc} ${Dat} ${Dtails}`)
+            alert("Is it printing")
+        });
+
+        del.addEventListener("click", ()=>{
+            console.log("Delete Button");
+            console.log(currentArray[i])
+        });
+
+        newDiv.innerHTML=`Topic:{${element.Topic}} <br> Date:{${element.Date}}<br>Details: {${element.Details}}`;
+        container.append(edt,del,newDiv)
+    });
+    // document.getElementById("Objecthere").innerHTML = itemsArray.map(displayArray).join("<br>");
+    // function displayArray(item){
+    //     return[item.Topic,item.Date,item.Details].join(" <br>");
+    // }   
+}
+function mainHead(){
+    const MainHeading = document.getElementById(MainHeading).value
+    console.log(MainHeading)
+    alert("MainHeading")
 }
