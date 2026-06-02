d8c84323b5e1157398276831c1e04e5fd3fe76bb
Ali Hussan Malik <malikalihussan2@gmail.com>
2026-06-02 17:27:19 +0500

    SeperateJS_Files
    The new file has solved the current problem. values in edit page are displayed.

---
+++ main.js
@@ -0,0 +1,40 @@
+const rawData = localStorage.getItem("Universal Key")
+console.log(`rawData${rawData}`)
+// alert(rawData)
+
+if(rawData) {
+    
+    const itemsArray = JSON.parse(rawData)
+    
+    const container = document.getElementById('data-container');
+    
+    
+    itemsArray.forEach((element, index) => {
+        const newDiv = document.createElement("div");
+        newDiv.className = 'newDivClass'
+        
+        const edt = document.createElement("button")
+        const del = document.createElement("button")
+        
+        edt.textContent = "Edit";
+        del.textContent = "Delete";
+        
+        edt.className = 'edit-button';
+        del.className = 'delete-button';
+        edt.addEventListener("click", () => {
+            window.location.href = `http://127.0.0.1:5500/todo.html?index=${index}`;
+        });
+        del.addEventListener("click", () => {
+            alert("Delete button clicked")
+            console.log("Delete Button");
+            console.log(currentArray[i])
+            document.removeChild(currentArray.index)
+        });
+        newDiv.innerHTML = `Topic:{${element.Topic}} <br> Date:{${element.Date}}<br>Details: {${element.Details}}`;
+        // alert("In Raw Data")
+        container.append(edt, del, newDiv);
+    });
+}
+else{
+    console.log("Else Part")
+}
--- todo.js
+++ todo.js
@@ -1,16 +1,50 @@
-    // let MainHeading = document.getElementsByClassName('MainHeading').value;
-    // console.log(`This is ${MainHeading.value}`)
-    // const queryString = window.location.search;
-    // const urlParams = new URLSearchParams(queryString)
-    
-    // const page = urlParams.get('${index}')
-    // console.log(`This is the page${page}`)
-    // alert("MainHeading")
-    // alert("Is it printing")
 
-function consoleIt() {
+console.log("Is it working?")
+alert("Is it working 01?")
+
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString)
+
+const page = urlParams.get('index')
+const NewHeading = document.getElementById("todoHeading")
+console.log(`QueryString {${queryString}} url Params {${urlParams}}`)
+alert(`page${page}`)
+
+const topicInput = document.getElementById('topicId')
+const dateInput = document.getElementById('dateId')
+const detailsInput = document.getElementById('detailsId')
+const submitButton = document.getElementById('submit')
+
+const SavedTasks = JSON.parse(localStorage.getItem("Universal Key")) || [];
+const taskIndex = parseInt(page);
+const currentTask = SavedTasks[taskIndex];
+console.log(` Query String {${queryString}} Url Params {${urlParams}} Page{${page}} NewHeading{${NewHeading}} SavedTasks {${SavedTasks}} Task Index {${taskIndex}} current Task {${currentTask}}`)
+alert(currentTask.Topic);
+alert(currentTask.Date);
+alert(currentTask.Details);
+
+alert(`Page Actual${page}`)
+if(page) {
+    alert("Is it working 01.01?")
+    console.log(`This is the Task ${page}`)
+    NewHeading.innerHTML = 'Edit Your Task';
+    submitButton.value = 'Update Task';
+
+    topicInput.value = currentTask.Topic;
+    dateInput.value = currentTask.Date;
+    detailsInput.value = currentTask.Details;
+}
+else {
+    alert(`Page is by passed`)
+    console.log("Page does not exist")
+}
+
+
+
+//Submit button of form 
+function handleSubmit() {
     let Topicx = document.getElementById("topicId").value;
-    let Datex = document.getElementById("dateid").value;
+    let Datex = document.getElementById("dateId").value;
     let Detailsx = document.getElementById("detailsId").value;
 
     function MyObject(tpc, dt, dtl) {
@@ -18,78 +52,16 @@ function consoleIt() {
         this.Date = dt;
         this.Details = dtl;
     }
-
     const NewList = new MyObject(Topicx, Datex, Detailsx);
 
     let storedData = localStorage.getItem("Universal Key");
     var currentArray = [];
-
-    if (storedData === null) {
-        currentArray.push(NewList);
-    } else {
-        currentArray = JSON.parse(storedData);
-
-        currentArray.push(NewList);
+        if(storedData === null) {
+            currentArray.push(NewList);
+        } else {
+            currentArray = JSON.parse(storedData);
+            currentArray.push(NewList);
+        }
+        localStorage.setItem("Universal Key", JSON.stringify(currentArray));
+        return;
     }
-
-    localStorage.setItem("Universal Key", JSON.stringify(currentArray));
-
-    // for (let i = 0; i < currentArray.length; i++) {
-    //     console.log(`Item ${i}: Topic: ${currentArray[i].Topic}, Date: ${currentArray[i].Date}`);
-    // }
-    return;
-}
-
-const rawData = localStorage.getItem("Universal Key")
-
-if (rawData) {
-
-    const itemsArray = JSON.parse(rawData)
-
-    const container = document.getElementById('data-container');
-
-
-    itemsArray.forEach((element, index) => {
-        const newDiv = document.createElement("div");
-        newDiv.className = 'newDivClass'
-
-        const edt = document.createElement("button")
-        const del = document.createElement("button")
-
-        edt.textContent = "Edit";
-        del.textContent = "Delete";
-
-        edt.className = 'edit-button';
-        del.className = 'delete-button';
-
-        edt.addEventListener("click", (topic, date, details) => {
-            window.location.href = `http://127.0.0.1:5500/todo.html?index=${index}`;
-        });
-
-        del.addEventListener("click", () => {
-            console.log("Delete Button");
-            console.log(currentArray[i])
-            document.removeChild(currentArray.index)
-        });
-
-        newDiv.innerHTML = `Topic:{${element.Topic}} <br> Date:{${element.Date}}<br>Details: {${element.Details}}`;
-        container.append(edt, del, newDiv)
-    });
-    // document.getElementById("Objecthere").innerHTML = itemsArray.map(displayArray).join("<br>");
-    // function displayArray(item){
-    //     return[item.Topic,item.Date,item.Details].join(" <br>");
-    // }   
-}
-// function mainHead() {
-//     const MainHeading = document.getElementById(MainHeading).value
-//     console.log(MainHeading)
-//     const queryString = window.location.search;
-//     const urlParams = new URLSearchParams(queryString)
-    
-//     const Topc = getElementById('Topic')
-//     const Dat = urlParams.get('Date')
-//     const Dtails = urlParams.get('Details')
-//     console.log(`${Topc} ${Dat} ${Dtails}`)
-//     alert("MainHeading")
-//     alert("Is it printing")
-// }
